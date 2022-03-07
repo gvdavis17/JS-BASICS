4d827296208f66872e5fd59ec6b282501563714f
“gvdavis17” <gvdavis17@gmail.com>
2022-03-07 13:45:31 -0500

    JSBASICS

---
+++ script.js
@@ -0,0 +1,74 @@
+// 1
+console.log(55);
+
+// 2
+/*
+Strings
+Boolean
+Numbers
+Null
+Undefined
+*/
+
+// 3
+// Addition is +
+// Subtraction is -
+// Multiplication is *
+// Division is /
+// Modulus is %
+// Exponentiation is **
+
+// 4
+console.log(5 % 4); // 1
+console.log(5 % 2); // 1
+
+// 5
+console.log('I\'M MARY POPPINS, Y\'ALL!');
+
+// 6
+console.log("Hawk" + "eye");
+
+// 7
+console.log("Gabrielle".length + "Davis".length); //14
+
+// 8
+console.log("Avengers: Endgame"[16]);
+
+
+// BONUS
+// 1
+console.log("\\");
+
+// 2
+console.log((5 ** 4) - 9);
+// OR
+// console.log(5 ** 4 - 9);
+
+// 3
+console.log("I".length + "Am".length % "Iron".length - "Man".length);
+
+console.log(("I".length + "Am".length + "Iron".length + "Man".length) % 10);
+
+console.log(("I".length - "Am".length) + ("Iron".length % "Man".length));
+
+// 4
+console.log("CAP"[1]);
+console.log("VISION"[0]);
+console.log("OKOYE"[4]);
+console.log("NICK FURY"[0]);
+console.log("GROOT"[0]);
+console.log("DOCTOR STRANGE"[13]);
+console.log("THOR"[3]);
+console.log("STAR-LORD"[0]);
+
+// OR
+console.log("CAP"[1] + "VISION"[0] + "OKOYE"[4] + "NICK FURY"[0] + "GROOT"[0] + "DOCTOR STRANGE"[13] + "THOR"[3] + "STAR-LORD"[0]);
+// OR
+lArray = ["CAP", "VISION", "OKOYE", "NICK FURY", "GROOT", "DOCTOR STRANGE", "THOR", "STAR-LORD"];
+console.log(lArray[0][1] + lArray[1][0] + lArray[2][4] + lArray[3][0] + lArray[4][0] + lArray[5][13] + lArray[6][3] + lArray[7][0]);
+
+// 5
+let m = 25;
+let c = 4;
+let u = m + c;
+console.log(u);
